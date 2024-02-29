6662e44bacebc219dcc471cdf39b79cd9f0e5b72
Rian-Noronha <riannoronha011@gmail.com>
2024-02-29 15:36:26 -0300

    Initial commit

---
+++ src/functions/Listing3_2.java
@@ -0,0 +1,20 @@
+package functions;
+
+public class Listing3_2 {
+
+    // -> Small,
+
+  /*  public static String renderPageWithSetupsAndTeardowns(
+            PageData pageData, boolean isSuite
+    )throws Exception{
+        boolean isTestPage = pageData.hasAttribute("Test");
+        WikiPage testPage = pageData.getWikiPage();
+        StringBuffer newPageContent = new StringBuffer();
+        includeSetupsPage(testPage, newPageContent, isSuite);
+        newPageContent.append(pageData.getContent());
+        includeTeardownPages(testPage, newPageContent, isSuite);
+        pageData.setContent(newPageContent.toStrin());
+
+        return pageData.getHtml();
+    }*/
+}
---
+++ src/functions/Listing3_3.java
@@ -0,0 +1,14 @@
+package functions;
+
+public class Listing3_3{
+    /*
+    * public static String renderPagesWithSetupsAndTeardowns(
+            PageData pageData, boolean isSuite
+    )throws Exception{
+        if(isTestPage(pageData)){
+            includeSetupAndTeardownPages(pageData, isSuite);
+        }
+
+        return pageData.getHtml();
+    }*/
+}
